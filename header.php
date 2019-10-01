76497d73955d0441a4d79d42c8b29e1bc9f139ee
Olaf Sarnow <olsarnow@gmail.com>
2019-10-01 22:01:12 +0200

    init

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<html>
+ <head>
+ 
+   
+  <title>sarnow.no/title>
+  
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+  <link rel=\"stylesheet\" href=\"osa.css\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale = 1.0,maximum-scale=1.0, user-scalable=no\" /> 
+
+  
+ 
+  <link href=\"https://fonts.googleapis.com/css?family=Montserrat+Alternates|Space+Mono|Pacifico&display=swap\" rel=\"stylesheet\" media=\"none\" onload=\"if(media!='all')media='all'\">
+  <noscript><link href=\"https://fonts.googleapis.com/css?family=Montserrat+Alternates|Space+Mono|Pacifico&display=swap\" rel=\"stylesheet\"></noscript>
+  
+  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+
+ </head>
+ 
+ ";
+ 
+ ?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('header.php'); ?>
+
+<body>
+      
+<div class=index>
+  
+<div class=indexbig>
+sarnow.no
+</div>
+
+div class=indexsmall>
+bergen,hordaland
+</div>
+
+</div>
+
+</body>
+</html>
+    
