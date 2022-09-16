718a148e3187101b863e97b9d7da0427b56fac85
Olaf Sarnow <o.sarnow@syseleven.de>
2022-09-16 11:55:49 +0200

    font changed

--- header.php
+++ header.php
@@ -15,10 +15,6 @@ echo "<html>
 <meta name=\”robots\” content=\"index, follow\">
 <meta name=\"description\" content=\"sarnow.no - bergen, vestland, norway\">
 
-<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
-<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-<link href=\"https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100;300&family=Noto+Sans+Runic&display=swap\" rel=\"stylesheet\">
-
  </head>
  
  ";
