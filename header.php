7b12fbc16d7e2ea29b29511281d637254a3c095c
Olaf Sarnow <o.sarnow@syseleven.de>
2022-09-16 11:38:14 +0200

    font changed

--- header.php
+++ header.php
@@ -5,12 +5,13 @@ echo "<html>
  
    
 <title>sarnow.no</title>
+
+<meta charset=\"UTF-8\">
   
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 <link rel=\"stylesheet\" href=\"osa.css\">
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 <meta name=\"viewport\" content=\"width=device-width, initial-scale = 1.0,maximum-scale=1.0, user-scalable=no\" /> 
 
-<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
 <meta name=\”robots\” content=\"index, follow\">
 <meta name=\"description\" content=\"sarnow.no - bergen, vestland, norway\">
 
