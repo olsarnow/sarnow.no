4c5abb83fdb5a7bf21b2480f4a03fbbafca3e930
Olaf Sarnow <o.sarnow@syseleven.de>
2022-09-16 13:33:55 +0200

    footer added

--- footer.php
+++ footer.php
@@ -7,6 +7,24 @@ echo "
 	<td>
 	<img class=footer src=./footerpics/s-20191231_094557448_iOS.jpg>
 	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20191231_102035931_iOS.jpg>
+	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20200101_113830619_iOS.jpg>
+	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20200727_143240786_iOS.jpg>
+	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20200731_103459755_iOS.jpg>
+	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20200808_191626918_iOS.jpg>
+	</td>
+	<td>
+	<img class=footer src=./footerpics/s-20200828_173329978_iOS.jpg>
+	</td>
 </tr>
 
 </table>
